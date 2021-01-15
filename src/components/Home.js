daf8b5aa611b893310ca0c251c045c4ccf1d32d8
Priya <pjainalwar@gmail.com>
2021-01-15 21:32:38 +0530

    Css changes for footer, btn

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <>
-      <h1> Fire Quiz</h1>
+      {/*<h1> Fire Quiz</h1> */}
       <Link to="/game" className="btn">
         Start Game
       </Link>
