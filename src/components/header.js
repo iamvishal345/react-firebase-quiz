819d0dd46c10938ef9e1c78ff8c0d2e83abcd29b
Priya <pjainalwar@gmail.com>
2021-01-16 15:41:36 +0530

    Color combination changed, Css fixes, Footer fixes

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 export default function Header(){
     return(
         <>
         <div id="top-heading">
-                <h2><span id="fire">Fire</span><span id="quiz">Quiz</span></h2>
+        <Link to="/">
+        <h2><span id="fire">Fire</span><span id="quiz">Quiz</span></h2>
+        </Link>
         </div>
         </>
     )
