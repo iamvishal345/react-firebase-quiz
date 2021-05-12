508927567c87a13ec2c0946188ae28e90ca3597f
iamvishal345 <vishalagnihotri345@gmail.com>
2021-05-12 19:32:36 +0530

    JSConfig update

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-export default function Header(){
-    return(
-        <>
-        <div id="top-heading">
+import React from "react";
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    <>
+      <div id="top-heading">
         <Link to="/">
-        <h2><span id="fire">Fire</span><span id="quiz">Quiz</span></h2>
+          <h2>
+            <span id="fire">Quiz</span>
+            <span id="quiz">Quiz</span>
+          </h2>
         </Link>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
